5465957f49e529f559c6a2d4b9001092e7a9a8be
I.P.S.Ashi <sayuris@openqubit.com>
2017-06-17 16:17:25 +0530

    done

---
+++ OpenQbit.HospitalSystem.git/OpenQbit.Hospital.Common.Utils.Logs/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Hospital.Common.Utils.Logs
+{
+    public class Logger :ILogger
+    {
+        public bool LogError(string err)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
