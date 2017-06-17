36ae528ce1933edf558e7f8ce04023bc9c07b630
abirandi gamage <abirandigamage96@gmail.com>
2017-06-17 16:12:20 +0530

    added

---
+++ OpenQbit.HospitalSystem.git/OpenQbit.HospitalSystem.Common.loc/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.HospitalSystem.Common.loc
+{
+    public class Class1
+    {
+    }
+}
