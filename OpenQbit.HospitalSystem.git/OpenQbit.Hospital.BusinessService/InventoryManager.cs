f05b7b912654f2bf86ee0701f2ee88e6ba96a7a0
Sanu Vithanage <sanuv9683@gmail.com>
2017-06-30 05:52:08 -0700

    All Done

--- OpenQbit.HospitalSystem.git/OpenQbit.Hospital.BusinessService/InventoryManager.cs
+++ OpenQbit.HospitalSystem.git/OpenQbit.Hospital.BusinessService/InventoryManager.cs
@@ -5,11 +5,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQbit.Hospital.Common.Models.Ward;
+using OpenQbit.Hospital.DataAccess.DAL.Contracts;
+using OpenQbit.Hospital.Common.Utils.Logs;
 
 namespace OpenQbit.Hospital.BusinessService
 {
     public class InventoryManager : IInventoryManager
     {
+        private IRepository _repository;
+
+        private ILogger _log;
+
+        [InjectionConstructor]    // Constructore  inject
+        public InvoiceManger(IRepository repository, ILogger log)
+        {
+            this._repository = repository;
+            this._log = log;
+        }
+
+
+        [Dependency] //for setters
+        public IRepository Repository
+        {
+            get { return _repository; }
+            set { _repository = value; }
+        }
+
+        [Dependency] //for setters
+        public ILogger Logger
+        {
+            get { return _log; }
+            set { _log = value; }
+        }
+
+
+        [InjectionMethod] // for methodes
+        public void SetRepository(IRepository repository)
+        {
+            _repository = repository;
+        }
+
         public bool RecoredInventory(Ward ward)
         {
             throw new NotImplementedException();
--- OpenQbit.HospitalSystem.git/OpenQbit.Hospital.BusinessService/OrderManager.cs
+++ OpenQbit.HospitalSystem.git/OpenQbit.Hospital.BusinessService/OrderManager.cs
@@ -5,11 +5,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQbit.Hospital.Common.Models.Phamarcy.OpenQbit.Hospital.Common.Models.Phamarcy;
+using OpenQbit.Hospital.DataAccess.DAL.Contracts;
+using OpenQbit.Hospital.Common.Utils.Logs;
 
 namespace OpenQbit.Hospital.BusinessService
 {
     public class OrderManager : IOrderManager
     {
+        private IRepository _repository;
+
+        private ILogger _log;
+
+        [InjectionConstructor]    // Constructore  inject
+        public InvoiceManger(IRepository repository, ILogger log)
+        {
+            this._repository = repository;
+            this._log = log;
+        }
+
+
+        [Dependency] //for setters
+        public IRepository Repository
+        {
+            get { return _repository; }
+            set { _repository = value; }
+        }
+
+        [Dependency] //for setters
+        public ILogger Logger
+        {
+            get { return _log; }
+            set { _log = value; }
+        }
+
+
+        [InjectionMethod] // for methodes
+        public void SetRepository(IRepository repository)
+        {
+            _repository = repository;
+        }
+
         public bool RecoredOrder(Drug drug)
         {
             throw new NotImplementedException();
