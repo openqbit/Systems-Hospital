28e652701f9aab953ee14e47322ed282f9069a44
sanuv9683 <sanuv9683@gmail.com>
2017-06-30 03:51:19 -0700

    Logger clases created

---
+++ OpenQbit.HospitalSystem.git/OpenQbit.Hospital.Common.Utils.Logs/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Hospital.Common.Utils.Logs
+{
+    public interface ILogger
+    {
+        bool LogError(string err);
+    }
+}
---
+++ OpenQbit.HospitalSystem.git/OpenQbit.Hospital.Common.Utils.Logs/LoggerB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Hospital.Common.Utils.Logs
+{
+    class LoggerB
+    {
+    }
+}
