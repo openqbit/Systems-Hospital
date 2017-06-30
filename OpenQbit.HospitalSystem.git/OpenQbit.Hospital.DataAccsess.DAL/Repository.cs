6380f0c7b99c8d7b1a981853f88c59bf171a4f59
Sanu Vithanage <sanuv9683@gmail.com>
2017-06-30 05:05:10 -0700

    All Fixed in repo

--- OpenQbit.HospitalSystem.git/OpenQbit.Hospital.DataAccsess.DAL/Repository.cs
+++ OpenQbit.HospitalSystem.git/OpenQbit.Hospital.DataAccsess.DAL/Repository.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using OpenQbit.Hospital.DataAccess.DAL.Contracts;
+using System.Data.Entity;
 
 namespace OpenQbit.Hospital.DataAccsess.DAL
 {
@@ -14,22 +15,38 @@ namespace OpenQbit.Hospital.DataAccsess.DAL
         private PharmacyContext _db = new PharmacyContext();
         public bool Create<T>(T obj) where T : class
         {
-            throw new NotImplementedException();
+            try
+            {
+                _db.Set<T>().Add(obj);
+                return true;
+            }
+            catch (Exception edb)
+            {
+                return false;
+            }
         }
 
         public bool Delete<T>(T obj) where T : class
         {
-            throw new NotImplementedException();
+            try
+            {
+                _db.Set<T>().Remove(obj);
+                return true;
+            }
+            catch (Exception edb)
+            {
+                return false;
+            }
         }
 
         public T Find<T>(Expression<Func<T, bool>> predicate) where T : class
         {
-            throw new NotImplementedException();
+            return _db.Set<T>().FirstOrDefault<T>(predicate);
         }
 
         public List<T> FindList<T>(Expression<Func<T, bool>> predicate) where T : class
         {
-            throw new NotImplementedException();
+            return _db.Set<T>().Where<T>(predicate).ToList();
         }
 
         public List<T> GetAll<T>() where T : class
@@ -39,12 +56,30 @@ namespace OpenQbit.Hospital.DataAccsess.DAL
 
         public bool Save()
         {
-            throw new NotImplementedException();
+            try
+            {
+                _db.SaveChanges();
+                return true;
+            }
+            catch (Exception edb)
+            {
+                return false;
+            }
         }
 
         public bool Update<T>(T obj) where T : class
         {
-            throw new NotImplementedException();
+            try
+            {
+                var edbstate = _db.Entry(obj);
+                _db.Set<T>().Attach(obj);
+                edbstate.State = EntityState.Modified;
+                return true;
+            }
+            catch (Exception edb)
+            {
+                return false;
+            }
         }
     }
 }
