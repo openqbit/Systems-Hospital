c3a3aeddf179733d03344deef1671aa76920e135
sanuv9683 <sanuv9683@gmail.com>
2017-06-30 04:58:15 -0700

    Repository is Going.. ;-(

--- OpenQbit.HospitalSystem.git/OpenQbit.Hospital.DataAccsess.DAL/Repository.cs
+++ OpenQbit.HospitalSystem.git/OpenQbit.Hospital.DataAccsess.DAL/Repository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,40 @@ namespace OpenQbit.Hospital.DataAccsess.DAL
 {
     public class Repository : IRepository
     {
+        private PharmacyContext _db = new PharmacyContext();
+        public bool Create<T>(T obj) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete<T>(T obj) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> FindList<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update<T>(T obj) where T : class
+        {
+            throw new NotImplementedException();
+        }
     }
 }
