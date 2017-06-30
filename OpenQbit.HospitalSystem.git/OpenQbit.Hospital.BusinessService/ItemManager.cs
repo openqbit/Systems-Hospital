1b2deb609ed93b56c8344c8bff8c6770c1b673e6
sanuv9683 <sanuv9683@gmail.com>
2017-06-30 04:49:56 -0700

    :-) Fixed

--- OpenQbit.HospitalSystem.git/OpenQbit.Hospital.BusinessService/ItemManager.cs
+++ OpenQbit.HospitalSystem.git/OpenQbit.Hospital.BusinessService/ItemManager.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using OpenQbit.Hospital.BusinessService.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQbit.Hospital.Common.Models;
 
 namespace OpenQbit.Hospital.BusinessService
 {
-    class ItemManager
+    public class ItemManager : IItemManager
     {
+        public bool RecoredManager(Item item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
