423d445401800643dcf3dea60368115f20022a27
abirandi gamage <abirandigamage96@gmail.com>
2017-07-01 12:38:01 +0530

    Patients Controller added

---
+++ OpenQbit.HospitalSystem.git/OpenQbit.Hospital.Presentation.Web/Controllers/Patients1Controller.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OpenQbit.Hospital.Common.Models;
+using OpenQbit.Hospital.DataAccsess.DAL;
+
+namespace OpenQbit.Hospital.Presentation.Web.Controllers
+{
+    public class Patients1Controller : Controller
+    {
+        private PharmacyContext db = new PharmacyContext();
+
+        // GET: Patients1
+        public ActionResult Index()
+        {
+            return View(db.Patient.ToList());
+        }
+
+        // GET: Patients1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patient.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // GET: Patients1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Patients1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,PatientName")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Patient.Add(patient);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(patient);
+        }
+
+        // GET: Patients1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patient.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patients1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,PatientName")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(patient).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(patient);
+        }
+
+        // GET: Patients1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patient.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patients1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Patient patient = db.Patient.Find(id);
+            db.Patient.Remove(patient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
