6056863ad13ff5aeb6103d27fba15164c7f65812
Thisara <thisaradakshina1996@gmail.com>
2017-07-07 11:07:48 +0530

    Modifing DAL and Model Class

--- OpenQbit.HospitalSystem.git/OpenQbit.Hospital.Common.Models/Room.cs
+++ OpenQbit.HospitalSystem.git/OpenQbit.Hospital.Common.Models/Room.cs
@@ -8,5 +8,10 @@ namespace OpenQbit.Hospital.Common.Models
 {
     public class Room
     {
+        public string RoomID { get; set; }
+        public string RoomType { get; set; }
+
+        public string Description { get; set; }
+
     }
 }
