1a342d0a3f168682d979b5280a3d16bbe11612c6
ravinudwijepala <ravinduw119@gmail.com>
2017-07-10 11:54:33 +0530

    create BLL  project

---
+++ OpenQbit.HospitalSystem.git/OpenQbit.Hospital.BusinessService.Contracts/IWardAdmitionManeger.cs
@@ -0,0 +1,20 @@
+﻿using OpenQbit.Hospital.Common.Models.Ward;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Hospital.BusinessService.Contracts
+{
+    public interface IWardAdmitionManeger
+    {
+        
+        bool RecoredWardAdmition(WardAdmition WardAdmition);
+        bool EditWardAdmition(WardAdmition WardAdmition);
+        bool DeleteWardAdmition(WardAdmition WardAdmition);
+        List<WardAdmition> GetAllWardAdmition();
+        WardAdmition FindWardAdmition(WardAdmition WardAdmition);
+        List<WardAdmition> FindWardAdmition(List<WardAdmition> WardAdmition);
+    }
+}
---
+++ OpenQbit.HospitalSystem.git/OpenQbit.Hospital.BusinessService/WardTypeManger.cs
@@ -0,0 +1,53 @@
+﻿using OpenQbit.Hospital.BusinessService.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQbit.Hospital.Common.Models;
+using OpenQbit.Hospital.DataAccess.DAL.Contracts;
+using OpenQbit.Hospital.Common.Utils.Logs;
+
+namespace OpenQbit.Hospital.BusinessService
+{
+    class WardTypeManger : IWardTypeManager
+    {
+        private IRepository _repository;
+
+        private ILogger _log;
+        public WardTypeManger(IRepository repository, ILogger log)
+        {
+            this._repository = repository;
+            this._log = log;
+        }
+        public bool DeleteWardType(WardType WardType)
+        {
+            _log.LogError("");
+            return _repository.Delete<WardType>(WardType);
+        }
+
+        public bool EditWardType(WardType WardType)
+        {
+            _log.LogError("");
+            return _repository.Update<WardType>(WardType);
+        }
+       
+        public WardType FindWardType(WardType WardType)
+        {
+            _log.LogError("");
+            return _repository.Find<WardType>(w => w.WardTypeId == WardType.WardTypeId);
+        }
+
+        public List<WardType> GetAllWardType()
+        {
+            _log.LogError("");
+            return _repository.GetAll<WardType>();
+        }
+
+        public bool RecoredWardType(WardType WardType)
+        {
+            _log.LogError("");
+            return _repository.Create<WardType>(WardType);
+        }
+    }
+}
