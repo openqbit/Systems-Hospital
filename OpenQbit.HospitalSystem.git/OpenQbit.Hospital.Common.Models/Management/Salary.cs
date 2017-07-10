0f466cba6b488c075d6bb1af7957c1b276f9b813
Umila Heshan <Umilaheshan07@gmail.com>
2017-07-10 12:26:49 +0530

    Create Attendance,Person,Employee,Salary Models

---
+++ OpenQbit.HospitalSystem.git/OpenQbit.Hospital.Common.Models/Management/Salary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Hospital.Common.Models.Management
+{
+   public class Salary
+    {
+        public int Id { get; set; }
+        public int SystemId { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime Date { get; set; }
+        public double Transport { get; set; }
+        public double OT { get; set; }
+        public double Tax { get; set; }
+        public double EPF { get; set; }
+        public double EF { get; set; }
+        public double NoPay { get; set; }
+
+        public virtual Employee Employee { get; set; }
+    }
+}
